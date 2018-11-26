fd15240b6201dd2a9a260fb946755f01cee689f3
norvylle <nsuy@up.edu.ph>
2018-11-26 19:48:46 +0800

    added README

---
+++ sample.js
@@ -0,0 +1,27 @@
+const { exec } = require('child_process');
+const express = require('express');
+const app = express();
+
+app.use(require('cors')());
+
+app.get('/',function(req,res){
+    if(req.query.letters != undefined && req.query.search != undefined){
+        execute(req.query.letters,req.query.search,res)
+    }else{
+        res.sendStatus(400)
+    }
+});
+
+app.listen(3001, () =>{
+    console.log("API is running at port 3001...");
+});
+
+async function execute(a,b,res){
+    console.log('> ./a.out '+a+' '+b)
+    exec('./a.out '+a+' '+b,(err, stdout, stderr) => {
+        if(err){
+            res.sendStatus(400);
+        }
+        res.send([stdout]);
+    })
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Menu, Input, Button  } from 'semantic-ui-react';
+const autoBind = require('auto-bind');
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      letters:'',
+      search:[],
+      message:''
+    }
+    autoBind(this)
+  }
+
+  async handleLetters(e){
+    await this.setState({letters: e.target.value});
+    this.setState({search: new Array(this.state.letters.length)})
+  }
+
+
+  handleSearch(e){
+    this.setState({search: e.target.value})
+  }
+
+  handleClick(){
+    fetch(`http://localhost:3001/?letters=${this.state.letters}&search=${this.state.search}`)
+    .then((response) => { return response.json()})
+    .then((data) => {this.setState({message:data[0]})})
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+          <Menu color='red' fixed='top' inverted borderless>
+              <Menu.Item color='white' header inverted>
+              Wordscrapes
+              </Menu.Item>
+          </Menu>
+          <div style={{padding: 50}}/>
+          <Input placeholder='Letters' onChange={this.handleLetters} maxLength="29"/>
+          <Input placeholder='Search' onChange={this.handleSearch} maxLength={this.state.letters.length}/>          
+          <Button content="Check State" onClick={this.handleClick}/>
+          <div style={{padding: 50}}/>
+          {this.state.message}
       </div>
     );
   }
